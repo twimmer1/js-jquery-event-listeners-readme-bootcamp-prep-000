ed8638d70a15b7f945af6a3d02b4a265761104cf
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-08 20:02:45 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
-//define functions here
+function getIt() {
+  $('p').on('click', function() {
+    alert("Hey!")
+  })
+}
+
+function frameIt() {
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  })
+}
+
+function pressIt() {
+  $(document).on('keydown', function(key) {
+  if(key.which == "G".charCodeAt(0)){
+      alert('G was pressed');
+  }
+});
+}
+
+function submitIt() {
+  
+}
 
 $(document).ready(function(){
 
-// call functions here
+getIt();
+frameIt();
+pressIt();
+
 
 });

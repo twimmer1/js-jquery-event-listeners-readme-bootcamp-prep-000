8391b50fbf66980e07bed6051852d20f27d3ff8e
Tristan Wimmer <tristan.a.wimmer@gmail.com>
2018-04-08 20:03:04 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -19,7 +19,14 @@ function pressIt() {
 }
 
 function submitIt() {
-  
+  $("form").on("submit", function() {
+  if ($( "input:first" ).val() === "correct") {
+    alert('your form is going to be submitted now');
+    return;
+  }
+  alert("you entered the wrong value");
+  return;
+});
 }
 
 $(document).ready(function(){
